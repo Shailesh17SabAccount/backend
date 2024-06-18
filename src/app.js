8ae3a79bbd8436eaab3fc6366c63d8e005e25618
Shailesh kale <shaileshkale87730@gmail.com>
2024-06-18 17:18:31 +0530

    backup project

---
+++ src/app.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import AssignLeadrouter from "./router/AssignLead.routes.js";
+import clientRoutes from "./router/clientRoutes.routes.js";
+import clientinfoRoutes from "./router/clients.js";
+import FromDataRoutes from "./router/FromData.routes.js";
+const app = express();
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(cookieParser());
+
+// Routes Declarations
+app.use("/api/v1/AssignLead", AssignLeadrouter);
+app.use("/api/v1/clients", clientRoutes);
+app.use("/api/v1/clientsinfo", clientinfoRoutes);
+app.use("/api/v1/FromData", FromDataRoutes);
+
+export { app };
